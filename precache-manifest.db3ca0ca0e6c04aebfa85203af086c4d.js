9f0788b15939eb4df49c6d0359e8e5ad0aefc49d
Gayane Mirzoyan <gayusha@gmail.com>
2020-10-05 22:10:40 -0700

    Updates

---
+++ precache-manifest.db3ca0ca0e6c04aebfa85203af086c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51ae42210f8c8b99ccb1dbc4d84f1d69",
+    "url": "/2_Counter_With_Buttons/index.html"
+  },
+  {
+    "revision": "6df34dc8d9c2b0c5e5a6",
+    "url": "/2_Counter_With_Buttons/static/css/main.24d4ad42.chunk.css"
+  },
+  {
+    "revision": "08fe9cad89067fd124d5",
+    "url": "/2_Counter_With_Buttons/static/js/2.6bd6a92b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/2_Counter_With_Buttons/static/js/2.6bd6a92b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6df34dc8d9c2b0c5e5a6",
+    "url": "/2_Counter_With_Buttons/static/js/main.40da2375.chunk.js"
+  },
+  {
+    "revision": "351a699314fd80c04178",
+    "url": "/2_Counter_With_Buttons/static/js/runtime-main.a49d7659.js"
+  }
+]);
